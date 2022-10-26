b575863347c6e32e6d5a3ecbbbcb48bc0d43b191
rnstphns <97855804+rnstphns@users.noreply.github.com>
2022-10-26 18:09:55 -0500

    add courses table

--- front-end/school-app/src/app/school-details.component.ts
+++ front-end/school-app/src/app/school-details.component.ts
@@ -4,32 +4,45 @@ import { SchooldbService, School, Teacher, Course, Student, mongoResponse } from
 @Component({
   selector: 'app-school-details',
   template: `
+    <div *ngIf="school_details == undefined">
     <!-- <input  placeholder = 'Search By School Initials' #searchInput> -->
     <!-- <button (click)="getSchool(searchInput.value)">Get School Data</button> -->
-    <!-- v Temporary button for ease of dev, reenable search bar later -->
+    <!-- v Temporary button for ease of dev, reenable search bar ^ later -->
     <button (click) = "getSchool('MIU')">Get MIU Data</button><br> 
+    </div>
     <div class="table"  *ngIf="school_details !== undefined; else empty_table_message">
         <label class="table-title">{{school_details.name || "school name missing"}}</label><br><br>
 
-        <label class="teacher-label">Teachers:</label>
-        
+        <label for="teacher-table" class="teacher-label">Teachers:</label>
         <table>
           <thead>
-            <th>id</th>
+            <th>ID</th>
             <th>Name</th>
             <th>Department</th>
             <th><button id="new-teacher-button"(click)="newTeacher(school_details.name)">  New  </button></th>
           </thead>
           <tr *ngFor="let t of school_details.teachers">
-            <td >{{t.id}}</td>
+            <td>{{t.id}}</td>
             <td>{{t.name}}</td>
             <td>{{t.department}}</td>
             
             <!-- would be nice to add a tooltip on single click -->
             <td><button id="delete" (dblclick)="deleteTeacher(t.id)">Delete</button></td>
           </tr>
+        </table><br>
+        <label for="course-table" class="course-label">Courses:</label>
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Students</th>
+          </thead>
+          <tr *ngFor="let c of school_details.courses">
+            <td>{{c.id}}</td>
+            <td>{{c.title}}</td>
+            <td><button id="view-students" (click)="viewStudentsInCourse(c.id)">View Students</button></td>
+          </tr>
         </table>
-        
     </div>
     <ng-template #empty_table_message>~No Data Loaded~</ng-template>
   `,
@@ -72,4 +85,7 @@ export class SchoolDetailsComponent implements OnInit {
     console.log('TODO - build a new teacher page')
   }
 
+  viewStudentsInCourse(course_id: string) {
+    console.log('TODO - build redirect to student page')
+  }
 }
